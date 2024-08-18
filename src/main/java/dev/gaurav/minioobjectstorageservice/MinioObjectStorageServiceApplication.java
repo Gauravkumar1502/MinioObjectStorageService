097294f74a84d7beac54b4edd583cde07f65b2a5
Gauravkumar1502 <gauravkumar15021999@gmail.com>
2024-08-18 11:08:35 +0530

    chore: initialize project

---
+++ src/main/java/dev/gaurav/minioobjectstorageservice/MinioObjectStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.gaurav.minioobjectstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioObjectStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioObjectStorageServiceApplication.class, args);
+	}
+
+}
