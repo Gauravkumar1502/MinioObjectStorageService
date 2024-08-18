bca4847107b59ed327c0da7909cd3660beba145b
Gauravkumar1502 <gauravkumar15021999@gmail.com>
2024-08-19 00:26:44 +0530

    chore: configuration MiniO

--- src/main/java/dev/gaurav/minioobjectstorageservice/config/MinioConfig.java
+++ src/main/java/dev/gaurav/minioobjectstorageservice/config/MinioConfig.java
@@ -1,2 +1,32 @@
-package dev.gaurav.minioobjectstorageservice.config;public class MinioConfig {
+package dev.gaurav.minioobjectstorageservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+
+@Configuration
+public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        try{
+            return MinioClient.builder()
+                    .endpoint(endpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
